38dd474b319be993e721bdf616f1424b203aa610
tryout255 <tryout3699@gmail.com>
2022-04-02 00:17:13 -0300

    feat(staging) Add data for use on project and types for the newly added data

---
+++ src/data/categories.ts
@@ -0,0 +1,19 @@
+import { Category } from '../types/categoriesType';
+
+export const categories: Category = {
+    food: {
+        title: 'feeding',
+        color: 'blue',
+        expense: true
+    },
+    rent: {
+        title: 'housing rental',
+        color: 'brown',
+        expense: true
+    },
+    salary: {
+        title: 'Salary of FloatCode.inc',
+        color: 'green',
+        expense: false
+    }
+}
---
+++ src/data/items.ts
@@ -0,0 +1,35 @@
+import { Item } from '../types/itemsType';
+
+export const items: Item[] = [
+    {
+        date: new Date(2022, 10, 15),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.99
+    },
+    {
+        date: new Date(2022, 10, 16),
+        category: 'food',
+        title: 'Burger King',
+        value: 28.00
+    },
+    {
+        date: new Date(2022, 10, 20),
+        category: 'food',
+        title: 'Subway',
+        value: 46.78
+    },
+    {
+        date: new Date(2022, 10, 30),
+        category: 'rent',
+        title: 'mansion',
+        value: 10000000.00
+    },
+    {
+        date: new Date(2022, 10, 30),
+        category: 'salary',
+        title: 'FloatCode.inc',
+        value: 120000
+    },
+
+]
