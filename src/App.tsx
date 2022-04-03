f18819a14c5cf4548dfc923ed852207faa6cfb57
tryout255 <tryout3699@gmail.com>
2022-04-03 16:00:09 -0300

    feat(staging) Added utilities to ease necessary questions like filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/itemsType';
+import { Category } from './types/categoriesType';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { filterListByMonth, getCurrentMonth } from './utils/dateFilter';
+
+
 
 const App = () => {
+  const [list, setList] = useState(items);//Geral list 
+  const [filtered, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(
+      filterListByMonth(list ,currentMonth)
+    );
+  }, [list, currentMonth]);
+
   return (
     <C.Container>
       <C.Header >
